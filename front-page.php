347b34ed99dc9326e6287b3f01a32eb6bcc5c716
jgerminario <jgerminario@gmail.com>
2015-02-26 15:50:53 -0800

    refactors fully into html5 with refactored css

--- front-page.php
+++ front-page.php
@@ -4,11 +4,10 @@ Template Name: Home
 */
 ?>
 <?php get_header(); ?>
-  <div id="content" class="front-margin">
+  <section class="content front-margin">
     <?php while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part( 'content', 'page' ); ?>
-      <?php endwhile;  ?>
+      <?php get_template_part( 'content', 'page' ); ?>
+    <?php endwhile;  ?>
     <img class="fullwrap" src="<?php echo get_template_directory_uri(); ?>/imgs/bench.jpg" />
-    </div>
-    </div>
-  </div>
+  </section>
+</main>
--- index.php
+++ index.php
@@ -1,15 +1,20 @@
  <?php get_header(); ?>
-<!-- <div id="main"> -->
-	<!-- <?php get_sidebar(); ?> -->
-	<div id="content">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<div class="post"><h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title(); ?>"><?php the_title(); ?></a></h1>
-			<?php the_content(__('read more »')); ?>
-			<p class="date"><?php the_time('n.j.y') ?></div>
-			<?php endwhile; else: ?>
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-		</div>
-	</div>
-	<!--<div id="delimiter">
-	</div>
-	<?php get_footer(); ?>
+ <section class="content">
+ 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+ 		<article>
+ 			<h1>
+ 				<a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title(); ?>"><?php the_title(); ?>
+ 				</a>
+ 			</h1>
+ 			<?php the_content(__('read more »')); ?>
+ 			<p class="date">
+ 				<?php the_time('n.j.y') ?>
+ 			</p>
+		</article>
+ 	<?php endwhile; else: ?>
+ 	<p>
+ 		<?php _e('Sorry, no posts matched your criteria.'); ?>
+ 	</p>
+ <?php endif; ?>
+</section>
+</main>
