7bb3714e9380d62e0e3393650887ffead66c37e3
jgerminario <jgerminario@gmail.com>
2015-02-26 15:21:46 -0800

    refactors header and sidebar, begins to add css best practices

--- front-page.php
+++ front-page.php
@@ -4,13 +4,11 @@ Template Name: Home
 */
 ?>
 <?php get_header(); ?>
-<!-- <div id="main"> -->
-  <!-- <div class="top-front"> -->
-  </div>
   <div id="content" class="front-margin">
     <?php while ( have_posts() ) : the_post(); ?>
         <?php get_template_part( 'content', 'page' ); ?>
       <?php endwhile;  ?>
-    <img class="fullwrap" src="/blog/wp-content/themes/germinario/imgs/bench.jpg" />
+    <img class="fullwrap" src="<?php echo get_template_directory_uri(); ?>/imgs/bench.jpg" />
+    </div>
     </div>
   </div>
--- header.php
+++ header.php
@@ -1,23 +1,26 @@
 <!doctype html>
 <html>
-<head>
 	<title>Germinar.io</title>
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Source+Code+Pro|Open+Sans' rel='stylesheet' type='text/css'>
-</head>
 <body>
 	<div class="wrapper">
     <div id="main">
-      <div id="sidebar">
+      <div class="sidebar">
         <header class="top-left">
           <div class="fixed">
-          <div class="logo"><a class="img" href="/"><img src="/blog/wp-content/themes/germinario/imgs/germinario.png" alt="germinar.io"></a></div>
-          <ul class="sidelinks">
-            <li><a href="#">about</a></li>
-            <li><a href="#">portfolio</a></li>
-            <li><a href="#">blog</a></li>
-          </ul>
+          <div class="logo"><a class="img" href="/"><img src="<?php echo get_template_directory_uri(); ?>/imgs/germinario.png" alt="germinar.io"></a></div>
+          <nav class="sidelinks">
+            <ul>
+              <li><a href="#">about</a></li>
+              <li><a href="#">portfolio</a></li>
+              <li><a href="#">blog</a></li>
+            </ul>
+          </nav>
           <div class="social">
             <a href="http://www.twitter.com/jgerminario"><img src="<?php echo get_template_directory_uri(); ?>/imgs/twitter.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/imgs/twitter_gr.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/imgs/twitter.png'" alt="Twitter"></a>
             <a href="http://www.linkedin.com/pub/jesse-germinario/15/163/b86/"><img onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/imgs/linkedin_gr.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/imgs/linkedin.png'" src="<?php echo get_template_directory_uri(); ?>/imgs/linkedin.png" alt="LinkedIn"></a>
